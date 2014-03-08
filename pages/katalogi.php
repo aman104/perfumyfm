cb2ec82b4bf35c27b199d3ddd0f2b9edb368361a
Paweł Sałajczyk <psalajczyk@gmail.com>
2014-03-08 14:44:13 +0100

    katalogi pl

--- pages/katalogi.php
+++ pages/katalogi.php
@@ -6,7 +6,7 @@
 
         <?php echo renderBreadcrumbs($host, $page, $pages); ?>
 
-<?php 
+<?php
     switch($_SESSION['lang'])
     {
         case 'ru' : echo '
@@ -19,16 +19,17 @@
 <a href="'.$host.'/files/plan-marketingowy-my.pdf" style="margin-left: 45px;" class="cat3_en cat_left">Plan marketing</a>
 '; break;
 default : echo '
-<a href="http://cdn.fmgroup.pl/download/katalog_makeup_7.pdf" style="height: auto;margin-left: 45px; float: left;" class="cat3_pl">Katalog MAKEUP 7 <br />Pobierz pdf</a>
-<a href="http://cdn.fmgroup.pl/download/KATALOG_PERFUMY_wiosna_2013.pdf" style="height: auto;margin-left: 45px; float: left;" class="cat1_pl">Katalog PERFUMY WIOSNA 2013 <br />Pobierz pdf</a>
-<a href="http://cdn.fmgroup.pl/download/katalog_for_home_8.pdf" style="height: auto;margin-left: 45px; float: left;" class="cat2_pl">Katalog FOR HOME 8.pdf <br />Pobierz pdf</a>
+<a href="http://cdn.fmgroup.pl/download/FM_GROUP_Katalog_Perfum_19.pdf" style="height: auto;margin-left: 45px; float: left;" class="cat3_pl">Katalog Perfum 19 <br />Pobierz pdf</a>
+<a href="http://cdn.fmgroup.pl/download/katalog_makeup_8.pdf" style="height: auto;margin-left: 45px; float: left;" class="cat1_pl">Katalog makeup 8 <br />Pobierz pdf</a>
+<a href="http://cdn.fmgroup.pl/download/KATALOG_FOR_HOME_jesien_2013.pdf" style="height: auto;margin-left: 45px; float: left;" class="cat2_pl">KATALOG FOR HOME jesien 2013.pdf <br />Pobierz pdf</a>
+<a href="http://cdn.fmgroup.pl/download/FM_GROUP_Moblie_Przewodnik_5i_10_2013.pdf" style="height: auto;margin-left: 45px; float: left;" class="cat6_pl">FM GROUP Moblie Przewodnik <br />Pobierz pdf</a>
 <a href="'.$host.'/files/plan_marketingowy_pl.pdf" style="height: auto;margin-left: 45px; float: left;" class="cat3_en cat_left">Plan marketingowy</a>
 <a href="'.$host.'/files/Katalog_AURILE_1.pdf" style="height: auto;margin-left: 45px; float: left;" class="cat4_pl cat_left">Katalog AURILE</a>
 ';
     }
-?>         
+?>
 
-         
+
     </div>
 
 
